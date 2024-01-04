eac1d88881017ffaa435ef1448371cfa5457110e
“ariel.duarte” <arielpy@gmail.com>
2024-01-04 00:15:54 -0300

    Added new pages on router app

---
+++ app/about/page.tsx
@@ -0,0 +1,6 @@
+
+export default function About() {
+  return (
+    <><span className="text-5xl">About</span></>
+  )
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,5 @@
+export default function Price() {
+  return (
+    <><span className="text-5xl">Price</span></>
+  )
+}
