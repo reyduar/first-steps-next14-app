d92cf2bed1d77c3139dfb5e4954f32c36f2ba6e1
“ariel.duarte” <arielpy@gmail.com>
2024-01-06 01:59:22 -0300

    Added dockerfile to create and run app image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      outputStandalone: true,
+    },
+  };
+  
+  module.exports = nextConfig;
